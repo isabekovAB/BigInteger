23c55f2ecc84265f280e679f3f915cb2314e5cde
isabekovAB <79275368+isabekovAB@users.noreply.github.com>
2023-03-14 03:58:54 +0300

    Create big_integer.hpp

---
+++ big_integer.hpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+class BigInt {
+ public:
+  BigInt();
+  BigInt(int64_t);
+  BigInt(std::string);
+  BigInt(std::vector<int>);
+  BigInt(const BigInt&);
+
+  friend bool operator==(const BigInt&, const BigInt&);
+  friend bool operator!=(const BigInt&, const BigInt&);
+  bool operator<(const BigInt&) const;
+  bool operator>(const BigInt&) const;
+  bool operator<=(const BigInt&) const;
+  bool operator>=(const BigInt&) const;
+
+  BigInt& operator=(const BigInt&);
+  BigInt operator-() const;
+  BigInt operator+=(const BigInt&);
+  BigInt operator-=(const BigInt&);
+  BigInt operator*=(const BigInt&);
+  BigInt operator/=(const BigInt&);
+  BigInt operator%=(const BigInt&);
+
+  friend BigInt operator+(const BigInt&, const BigInt&);
+  friend BigInt operator-(const BigInt&, const BigInt&);
+  friend BigInt operator*(const BigInt&, const BigInt&);
+  friend BigInt operator/(const BigInt&, const BigInt&);
+  friend BigInt operator%(const BigInt&, const BigInt&);
+
+  BigInt operator++();
+  BigInt operator++(int);
+  BigInt operator--();
+  BigInt operator--(int);
+
+  BigInt& GeneralDiv(const BigInt&);
+  BigInt& DeleteZeros();
+  BigInt& OneSignPlus(const BigInt&);
+  BigInt& DiffSignPlus(const BigInt&);
+
+  friend std::ostream& operator<<(std::ostream&, const BigInt&);
+  friend std::istream& operator>>(std::istream&, BigInt&);
+
+ private:
+  std::vector<int> number_;
+  bool is_negative_;
+  bool is_null_;
+  const int kBase = 10;
+};
